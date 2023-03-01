fcf425770420a76363360d541ffa0a3a9b2aec1a
Payton Boone <paytonboone30@gmail.com>
2023-03-01 12:32:47 -0700

    looking fresh

--- checkpoint-6.client/src/pages/HomePage.vue
+++ checkpoint-6.client/src/pages/HomePage.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="container-fluid">
-    <div class="row bg-image text-light">
-      <div class="col-12 m-5 fs-4">
-        <div class="mb-2">
-          Tired of all those scalping nerds?
+    <div class="row text-light my-3">
+      <div class="col-12 bg-image">
+        <div class="row ms-3">
+          <div class="col-12 my-5 fs-4">
+            <div class="mb-3">
+              Tired of all those scalping nerds?
+            </div>
+            <div class="mb-3">
+              Reserve your spot NOW with
+            </div>
+            <div class="mb-3">
+              real events with true nerds.
+            </div>
+          </div>
         </div>
-        <div class="mb-2">
-          Reserve your spot NOW with
+      </div>
+    </div>
+    <div class="row mb-3">
+      <div class="col-12 m-auto">
+        <div class="types-color p-3 d-flex justify-content-around">
+          <button class="btn btn-outline-light">Concert</button>
+          <button class="btn btn-outline-light">Convention</button>
+          <button class="btn btn-outline-light">Digital</button>
+          <button class="btn btn-outline-light">Sport</button>
         </div>
-        <div class="mb-2">
-          real events with true nerds.
-        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 m-auto">
+
       </div>
     </div>
   </div>
@@ -28,5 +47,11 @@ export default {
 .bg-image {
   background-image: url(https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnQlMjBzZWF0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60);
   object-fit: cover;
+}
+
+
+
+.types-color {
+  background-color: #383b46;
 }
 </style>
