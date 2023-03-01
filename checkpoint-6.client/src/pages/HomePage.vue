dc6f1272054f3abbff98756dea1cb8581a3293b2
Payton Boone <paytonboone30@gmail.com>
2023-03-01 12:01:08 -0700

    basic styling ready

--- checkpoint-6.client/src/pages/HomePage.vue
+++ checkpoint-6.client/src/pages/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row bg-image text-light">
+      <div class="col-12 m-5 fs-4">
+        <div class="mb-2">
+          Tired of all those scalping nerds?
+        </div>
+        <div class="mb-2">
+          Reserve your spot NOW with
+        </div>
+        <div class="mb-2">
+          real events with true nerds.
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,23 +25,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.bg-image {
+  background-image: url(https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnQlMjBzZWF0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60);
+  object-fit: cover;
 }
 </style>
