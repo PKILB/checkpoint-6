ee8e4b3bd09d80b23173a60b2ec066ddbd717f2b
Payton Boone <paytonboone30@gmail.com>
2023-02-28 20:24:07 -0700

    back end can create an event

---
+++ checkpoint-6/server/models/Event.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+
+
+export const EventSchema = new Schema(
+    {
+    name: { type: String, required: true, minLength: 2, maxLength: 20},
+    description: { type: String, required: true, minLength: 20, maxLength: 2000},
+    coverImg: { type: String, required: true, maxLength: 5000},
+    location: { type: String, required: true, minLength: 10, maxLength: 150},
+    capacity: { type: Number, min: 10, max: 10000},
+    startDate: { type: Date, required: true},
+    isCanceled: { type: Boolean, default: false, required: true},
+    type: { type: String, enum: ['concert', 'convention', 'sport', 'digital'], required: true},
+
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true}
+}, { timestamps: true, toJSON: { virtuals: true }})
+
+
+EventSchema.virtual('creator', {
+    ref: 'Account',
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true
+})
