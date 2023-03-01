8c9f545d968e7656d7e42f3346f832103cb8684c
Payton Boone <paytonboone30@gmail.com>
2023-02-28 22:27:56 -0700

    can get event by id in the backend

--- checkpoint-6/server/services/EventsService.js
+++ checkpoint-6/server/services/EventsService.js
@@ -1,9 +1,18 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 
 
 class EventsService {
 
+    async getEventById(eventId) {
+        const event = await dbContext.Events.findById(eventId).populate('creator', 'name picture')
+
+        if (!event) {
+            throw new BadRequest('Invalid Event Id Dude')
+        }
+        return event
+    }
     async getAllEvents() {
         const events = await dbContext.Events.find()
         .populate('creator', 'name picture')
